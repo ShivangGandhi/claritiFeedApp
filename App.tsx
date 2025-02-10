66d92da85e6c37fcc08b6d1f26d4722d17297d10
Shivang Gandhi <shivanggandhi19@gmail.com>
2025-02-10 01:50:36 -0400

    Folder chnages

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { Provider } from 'react-redux';
 import { store } from './src/redux/store';
-import FeedScreen from './src/screen/FeedScreen';
+import FeedScreen from './src/screens/FeedScreen';
 import ErrorBoundary from './src/wrappers/ErrorBoundary';
 
 export default function App() {
